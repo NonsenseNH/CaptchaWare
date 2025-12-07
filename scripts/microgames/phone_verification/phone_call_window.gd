f8e0c37f3df4be5f92b4b3fe3edd93b948abff8a
NonsenseNH <86915512+NonsenseNH@users.noreply.github.com>
2025-12-07 19:56:26 +0800

    added voicelines for phone call

--- scripts/microgames/phone_verification/phone_call_window.gd
+++ scripts/microgames/phone_verification/phone_call_window.gd
@@ -1,15 +1,32 @@
 extends Control
 
+var correct_one: bool = false
+
+var cur_number_text: String = ""
+
+@onready var phone_number_node: Label = $PhoneNumber
 @onready var anim: AnimationPlayer = $anim
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var phone_audio: AudioStreamPlayer = $phoneAudio
 
+signal call_answered()
+signal call_declined()
+
+var answered: bool = false
 
 func _on_decline_pressed() -> void:
+	call_declined.emit()
 	queue_free()
 
-
 func _on_accept_pressed() -> void:
-	anim.play("answered")
+	if !answered:
+		anim.play("answered")
+		phone_audio.play()
+		call_answered.emit()
+	else:
+		queue_free()
+	answered = true
+
+
+func _on_phone_audio_finished() -> void:
+	queue_free()
--- scripts/microgames/phone_verification/phone_verification.gd
+++ scripts/microgames/phone_verification/phone_verification.gd
@@ -1,10 +1,64 @@
 extends Microgame
 
+const PHONE_CALL_WINDOW = preload("res://instances/PhoneVerification/phone_call_window.tscn")
 @onready var phonenumber_label: Label = $phonenumber
 var phone_number: int = 0
+
+var real_number_calling : bool = false
+var has_answered: bool = false
 # Called when the node enters the scene tree for the first time.
+
+var fake_number_type : PackedStringArray = [
+	"ding",
+	"erynn",
+	"hira",
+	"jackson",
+	"jam",
+	"juhin",
+	"julnz",
+	"marcz",
+	"miel",
+	"mystery",
+	"nonsense",
+	"puppet",
+	"zac"
+]
+
 func _ready() -> void:
 	phonenumber_label.text = generate_number()
+
+	await get_tree().create_timer(1).timeout
+
+	pop_up_window()
+
+
+func pop_up_window() -> void: #spawn window
+	var phone_call_window : Control = PHONE_CALL_WINDOW.instantiate()
+	var correct_one = bool(randi_range(0, 1))
+	
+	phone_call_window.call_answered.connect(on_call_answered)
+	phone_call_window.call_declined.connect(on_call_declined)
+	
+	add_child(phone_call_window)
+
+	if correct_one:
+		phone_call_window.phone_number_node.text = phonenumber_label.text
+		real_number_calling = true
+	else:
+		phone_call_window.phone_number_node.text = generate_number()
+		phone_call_window.phone_audio.stream = get_phone_call_audio()
+
+func get_phone_call_audio() -> AudioStream:
+	var path : String = "res://sounds/microgames/phone_verification/"
+
+	if real_number_calling:
+		path += "automated_message.mp3"
+	else:
+		path += "fake_numbers/" + fake_number_type[randi_range(0, fake_number_type.size() - 1)]
+		var audio_phone_final : PackedStringArray = get_file_list(path, ".mp3")
+		path += "/" + audio_phone_final[randi_range(0, audio_phone_final.size() - 1)]
+	
+	return load(path)
 
 func generate_number() -> String:
 	var number_text: String = ""
@@ -27,7 +81,21 @@ func get_phone_format(i: int) -> String:
 			return ""
 
 func canSkip() -> bool:
-	return false
+	return has_answered
 
 func isWinning() -> bool:
-	return super.isWinning()
+	return has_answered && real_number_calling
+
+
+func on_call_answered() -> void:
+	has_answered = true
+
+func on_call_declined() -> void:
+	if real_number_calling:
+		print_debug("FAILED")
+		return
+	
+	await get_tree().create_timer(1).timeout
+
+	pop_up_window()
+	
