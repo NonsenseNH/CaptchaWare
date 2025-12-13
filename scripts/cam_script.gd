f8e8fab3b38f152954abac80b1b95c488da772c0
NonsenseNH <86915512+NonsenseNH@users.noreply.github.com>
2025-12-14 01:39:31 +0800

    coded some face_right_up stuffs

--- scripts/cam_script.gd
+++ scripts/cam_script.gd
@@ -6,6 +6,15 @@ class_name cam_effects
 
 var shake_amount : float = 0
 var shake_vector : Vector2 = Vector2.ZERO
+var parallax_offset := Vector2.ZERO
+@onready var default_zoom :Vector2 = zoom
+
+var force_pause : bool = false
+
+#region Effects
+func pause_game(paused : bool) -> void: #use this function to prevent hitstop from unpausing the tree
+	force_pause = paused
+	get_tree().paused = paused
 
 func shake_camera(intensity:float, duration:float) -> void:
 	if duration == 0:
@@ -13,11 +22,31 @@ func shake_camera(intensity:float, duration:float) -> void:
 	var shake_amount_tween : Tween = create_tween()
 	shake_amount_tween.tween_property(self, "shake_amount", 0, duration).from(shake_amount + intensity).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
+func camera_bop_in(zoom_amount: float, duration:float) -> void:
+	var bop_tween : Tween = create_tween()
+	bop_tween.tween_property(self, "zoom", default_zoom, duration).from(Vector2.ONE * zoom_amount).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+
 func camera_shake_process() -> void:
 	if shake_amount == 0: return
-	shake_vector = Vector2(randf_range(-shake_amount,shake_amount), randf_range(-shake_amount,shake_amount))
-	offset += shake_vector
+	var final_amount : float = shake_amount
+	shake_vector = Vector2(randf_range(-final_amount,final_amount), randf_range(-final_amount,final_amount))
+
+func parallax_system() -> void:
+	if parallax_offset_amount == 0: return
+	parallax_offset = lerp(Vector2.ZERO, get_local_mouse_position(), parallax_offset_amount / 100.0)
+
+func hit_stop(duration: float) -> void:
+	get_tree().paused = true
+	await get_tree().create_timer(duration).timeout
+	get_tree().paused = force_pause
+#endregion
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		parallax_system()
+
+func _physics_process(_delta: float) -> void:
+	camera_shake_process()
 
 func _process(_delta: float) -> void:
-	offset = lerp(Vector2.ZERO, get_local_mouse_position(), parallax_offset_amount / 100.0)
-	camera_shake_process()
+	offset = shake_vector + parallax_offset
--- scripts/microgames/face_right_up/face_right_up.gd
+++ scripts/microgames/face_right_up/face_right_up.gd
@@ -1,11 +1,72 @@
 extends Microgame
 
+const IMAGES_FOLDER = "res://sprites/face_right_up/images/"
+const DOT_INSTANCE = "res://instances/faceUpRight/dot.tscn"
+const ROTATE_INTERVAL = 22.5
+
+@onready var hbox_container: HBoxContainer = $HBoxContainer
+@onready var the_ball: Node2D = $Circle
+@onready var anims: AnimationPlayer = $anims
+
+var image_pool : PackedStringArray = [
+	"test"
+]
+
+var how_many_balls : int = 1
+var current_ball : int = 1
+
+var ball_spin_dir : int = 0
+
+var cur_degrees : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var target_rotation : float = 0.0
 
+	while target_rotation == 0.0:
+		target_rotation = (randi_range(-7, 7) * ROTATE_INTERVAL)
+	
+	the_ball.rotation_degrees = (randi_range(-7, 7) * ROTATE_INTERVAL)
+	cur_degrees = the_ball.rotation_degrees
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	for i in range(how_many_balls):
+		var dot_instance = load(DOT_INSTANCE).instantiate()
+		hbox_container.add_child(dot_instance)
+
+func get_all_images() -> void:
+	image_pool = get_file_list(IMAGES_FOLDER)
+
+func push_ball() -> void:
+	if anims.is_playing():
+		anims.stop()
+	anims.play("bounce")
+
+	var ball_tween := create_tween()
+	
+	var target_rotation : float = cur_degrees + (ROTATE_INTERVAL * ball_spin_dir)
+
+	ball_tween.tween_property(the_ball, "rotation_degrees", target_rotation, 0.4).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	print_debug("Target Rotation: " + str(target_rotation))
+
+	cur_degrees = target_rotation
+
+func done_check() -> void:
+	var normalized_rotation : float = fmod(cur_degrees, 360.0)
+	
+	if normalized_rotation == 0.0:
+		print_debug("yes")
+
+func switch_image() -> void:
 	pass
+
+func _on_arrow_right_pressed() -> void:
+	ball_spin_dir = -1
+	push_ball()
+
+	done_check()
+
+func _on_arrow_left_pressed() -> void:
+	ball_spin_dir = 1
+	push_ball()
+
+	done_check()
